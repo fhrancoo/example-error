5ca90b3250b3e1cf6feeb5320ca219d4afdb302e
fhrancoo <132franco@gmail.com>
2024-05-02 20:05:01 -0300

    update .gitignore, env.d.ts and src/pages/index.astro

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly VARIABLE_1: string;
+  readonly VARIABLE_2: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
