c2a024aa8dfbc6f97716da20a0fc04df2d8b7736
fhrancoo <132franco@gmail.com>
2024-05-03 20:53:03 -0300

    update package.json, env.d.ts and src/pages/index.astro

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,22 @@
 /// <reference types="astro/client" />
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
+declare namespace App {
+  interface Locals {
+    runtime: any;
+  }
+}
+
+interface Locals extends Runtime {
+  otherLocals: {
+    test: string;
+  };
+  env: {
+    VARIABLE_1: string;
+    VARIABLE_2: string;
+  };
+}
+
 interface ImportMetaEnv {
   readonly VARIABLE_1: string;
   readonly VARIABLE_2: string;
